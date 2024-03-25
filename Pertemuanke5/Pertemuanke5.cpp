f86340c0cae55bdffa7016b8ac12b99a0053e1cf
Nurseto10 <adibseto476@gmail.com>
2024-03-25 11:50:22 +0800

    switch pilihan

--- Pertemuanke5/Pertemuanke5.cpp
+++ Pertemuanke5/Pertemuanke5.cpp
@@ -28,3 +28,18 @@ int pembagian(int bil_1, int bil_2) {
 	return bil_1 / bil_2;
 }
 
+
+int main()
+{
+	int pilihan;
+
+	do {
+		system("cls");
+		cout << "========== menu ==========" << endl;
+		cout << "1. penjumlahan" << endl;
+		cout << "2. pengurangan" << endl;
+		cout << "3. perkalian" << endl;
+		cout << "4. pembagian" << endl;
+		cout << "5. keluar" << endl;
+		cin >> pilihan;
+
